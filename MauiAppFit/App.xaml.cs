c3f704932b3af22c4790ebfad803ab306a40045e
Tiago <tiago@tiago.blog.br>
2025-05-14 19:33:47 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppFit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
